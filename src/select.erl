f784b584e54da9170af9bb95bb77677527ba4f29
Joao <jpd.sousa@campus.fct.unl.pt>
2017-05-17 19:24:39 +0100

    Refactored select

--- src/select.erl
+++ src/select.erl
@@ -13,20 +13,6 @@ exec(Table, Select) ->
 	{ok, TName} = tables:name(Table),
 	{ok, _Projection} = query_utils:search_clause(keys, Select),
 	{ok, Condition} = query_utils:search_clause(where, Select),
-	scan(TName, Condition, []).
-
-%% ====================================================================
-%% Internal functions
-%% ====================================================================
-
-scan(TableName, [{{atom_value, _Left}, Arop, {_AQLType, Str}} | Tail], Acc) ->
-	case Arop of
-		{assignment, "="} ->
-			NewAcc = lists:append(Acc, element:new(Str, TableName)),
-			scan(TableName, Tail, NewAcc);
-		_Else ->
-			{err, "Not supported yet! :)"}
-	end;
-scan(_TName, [], Acc) ->
-	{ok, Results, _CT} = antidote:read_objects(Acc),
+	{ok, Keys} = where:scan(TName, Condition),
+	{ok, Results, _CT} = antidote:read_objects(Keys),
 	{ok, Results}.

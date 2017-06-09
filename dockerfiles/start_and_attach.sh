401f4d5d33ca9ae6cbc08045b00c7f6f3dae7337
Joao <jpd.sousa@campus.fct.unl.pt>
2017-06-09 14:09:59 +0100

    Updated docker image
    Added the makefile command to automatically start docker with the AQL shell

--- dockerfiles/start_and_attach.sh
+++ dockerfiles/start_and_attach.sh
@@ -5,7 +5,7 @@ NPROC=$(nproc)
 if [ "$NPROC" -ge 2 ]; then
   trap "echo 'Shutting down' && /opt/antidote/bin/env stop" TERM
   /opt/antidote/bin/env start && sleep 10 && tail -f /opt/antidote/log/console.log &
-  sleep 10 && cd /opt/AQL && ./rebar3 shell --name=$AQL_NAME --setcookie antidote 
+  sleep 10 && cd /opt/AQL && make shell
 else
   echo "You need at least 2 cpu cores in your Docker (virtual) machine to run this image!"
   echo "You have ${NPROC} processors"

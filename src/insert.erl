b08945a7f2f3f34a324cfde54456f0a6586e84f9
Joao <jpd.sousa@campus.fct.unl.pt>
2017-06-22 18:46:37 +0100

    Fix merge

--- src/insert.erl
+++ src/insert.erl
@@ -1,7 +1,60 @@
+%% @author Joao
+%% @doc @todo Add description to select.
 
-	Update = crdt:map_update(ParentKey, [StOp, RefOp]),
-	antidote:update_objects(Update, TxId).
 
+-module(insert).
+
+-define(NO_PK, none).
+
+-include("aql.hrl").
+-include("parser.hrl").
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([exec/2]).
+
+exec(Table, Props) ->
+	Keys = query_utils:search_clause(?PROP_COLUMNS, Props),
+	Values = query_utils:search_clause(?PROP_VALUES, Props),
+	AnnElement = element:new(Table),
+	{ok, Element} = element:put(Keys, Values, AnnElement),
+	AntidoteOp = element:create_db_op(Element),
+	{ok, TxId} = antidote:start_transaction(),
+	antidote:update_objects(AntidoteOp, TxId),
+	% update foreign key references
+	Pk = element:primary_key(Element),
+	Fks = element:foreign_keys(Element),
+	lists:foreach(fun (Fk) -> update_ref(Fk, Pk, TxId) end, Fks),
+	% end
+	Res = antidote:commit_transaction(TxId),
+	case Res of
+		{ok, _CT} ->
+			ok;
+		_Else ->
+			Res
+	end.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+get_child(ParentKey, TxId) ->
+	{ok, [Parent]} = antidote:read_objects(ParentKey, TxId),
+	RefsKey = element:refs_key(),
+	Children = proplists:get_value(RefsKey, Parent, []),
+	io:fwrite("Parent: ~p~nChildren: ~p~n", [Parent, Children]),
+	Children.
+
+update_ref(ParentKey, ChildKey, TxId) ->
+	% update child tree
+	Children = get_child(ParentKey, TxId),
+	lists:foreach(fun (C) -> update_child(C, TxId) end, Children),
+	% update itself
+	RefsKey = element:refs_key(),
+	StKey = element:st_key(),
+	RefOp = crdt:field_map_op(RefsKey, crdt:add_all(ChildKey)),
+	StOp = crdt:field_map_op(StKey, crdt:assign_lww(ipa:touch())),
 	Update = crdt:map_update(ParentKey, [StOp, RefOp]),
 	antidote:update_objects(Update, TxId).
 
@@ -13,13 +66,3 @@ update_child(ParentKey, TxId) ->
 	StKey = element:st_key(),
 	StOp = crdt:field_map_op(StKey, crdt:assign_lww(ipa:touch_cascade())),
 	antidote:update_objects(crdt:map_update(ParentKey, StOp), TxId).
-
-get_keys(Table, Props) ->
-	Clause = query_utils:search_clause(?PROP_COLUMNS, Props),
-	case Clause of
-		?PARSER_WILDCARD ->
-			Keys = table:get_col_names(Table),
-			lists:map(fun (V) -> ?PARSER_ATOM(V) end, Keys);
-		_Else ->
-			Clause
-	end.

e03adff837d2051ecd53bc904fad501d4918e9d1
Joao <jpd.sousa@campus.fct.unl.pt>
2017-06-28 13:42:59 +0100

    Remove from parent on delete

--- src/delete.erl
+++ src/delete.erl
@@ -11,5 +11,15 @@ exec(Table, Props) ->
 	Keys = where:scan(TName, Condition),
   StKey = element:st_key(),
   StOp = crdt:field_map_op(StKey, crdt:assign_lww(ipa:delete())),
+  {ok, TxId} = antidote:start_transaction(),
+  remove_from_parent(),
   Updates = crdt:map_update(Keys, crdt:assign_lww()),
   antidote:update_objects(Updates).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+remove_from_parent() ->
+	RefsKey = element:refs_key(),
+	RefOp = crdt:field_map_op(RefsKey, crdt:add_all(ChildKey)),

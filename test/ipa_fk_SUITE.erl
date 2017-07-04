c41fcf0c6d8c4b53eb9769a493465a47b7e660ea
Joao <jpd.sousa@campus.fct.unl.pt>
2017-07-04 18:42:13 +0100

    Updated common tests

--- test/ipa_fk_SUITE.erl
+++ test/ipa_fk_SUITE.erl
@@ -4,18 +4,21 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+-compile(export_all).
+
 -export([init_per_suite/1,
           end_per_suite/1,
           init_per_testcase/2,
           end_per_testcase/2,
           all/0]).
 
--export([insert_test/1,
-          delete_test/1]).
+-export([insert_basic/1, insert_multilevel/1,
+          delete_basic/1, delete_multilevel/1]).
 
 init_per_suite(Config) ->
-  tutils:aql("CREATE TABLE FkB (id INT PRIMARY KEY);"),
-  tutils:aql("CREATE TABLE FkA (id INT PRIMARY KEY, b INT FOREIGN KEY REFERENCES B(id));"),
+  tutils:create_single_table("FkA"),
+  tutils:create_fk_table("FkB", "FkA"),
+  tutils:create_fk_table("FkC", "FkB"),
   Config.
 
 end_per_suite(Config) ->
@@ -28,16 +31,58 @@ end_per_testcase(_, _) ->
 ok.
 
 all() ->
-  [insert_test,
-  delete_test].
+  [insert_basic,
+  insert_multilevel,
+  delete_basic,
+  delete_multilevel].
 
-insert_test(_Config) ->
-  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1)"),
-  {ok, [Res]} = tutils:aql("SELECT * FROM FkB WHERE id = 1"),
-  ?assertEqual(ipa:new(), element:st_value(Res)).
+insert_basic(_Config) ->
+  % insert test
+  {ok, []} = tutils:aql("INSERT INTO FkA VALUES (1)"),
+  tutils:assertState(ipa:new(), "FkA", "1"),
+  % touch test
+  io:fwrite("B: ~p~n", [tutils:aql("SELECT * FROM FkB WHERE ID = 1")]),
+  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
+  io:fwrite("A: ~p~n", [tutils:aql("SELECT * FROM FkB WHERE ID = 1")]),
+  tutils:assertState(ipa:new(), "FkB", "1"),
+  tutils:assertState(ipa:touch(), "FkA", "1"),
+  % touch cascade test
+  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (2, 1)"),
+  tutils:assertState(ipa:touch(), "FkA", "1"),
+  tutils:assertState(ipa:touch_cascade(), "FkB", "1").
 
-delete_test(_Config) ->
-  {ok, []} = tutils:aql("INSERT INTO FKB VALUES (1)"),
-  {ok, []} = tutils:aql("DELETE FROM B WHERE id = 1"),
-  {ok, [Res]} = tutils:aql("SELECT * FROM FkB WHERE id = 1"),
-  ?assertEqual(ipa:delete(), element:st_value(Res)).
+insert_multilevel(_Config) ->
+  %bottom level insert
+  load_multilevel_data(),
+  tutils:assertState(ipa:touch(), "FkA", "1"),
+  tutils:assertState(ipa:touch(), "FkB", "1"),
+  tutils:assertState(ipa:touch_cascade(), "FkB", "2"),
+  tutils:assertState(ipa:touch_cascade(), "FkC", "1"),
+  tutils:assertState(ipa:new(), "FkC", "2"),
+  % middle level insert
+  tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
+  tutils:assertState(ipa:touch_cascade(), "FkC", "1"),
+  tutils:assertState(ipa:touch_cascade(), "FkC", "2").
+
+delete_basic(_Config) ->
+  {ok, []} = tutils:aql("INSERT INTO FkA VALUES (1)"),
+  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
+  {ok, []} = tutils:delete_by_key("FkA", "1"),
+  tutils:assertState(ipa:delete(), "FkA", "1"),
+  tutils:assertState(ipa:delete_cascade(), "FkB", "1").
+
+delete_multilevel(_Config) ->
+  load_multilevel_data(),
+  {ok, []} = tutils:delete_by_key("FkA", "1"),
+  tutils:assertState(ipa:delete(), "FkA", "1"),
+  tutils:assertState(ipa:delete_cascade(), "FkB", "1"),
+  tutils:assertState(ipa:delete_cascade(), "FkB", "2"),
+  tutils:assertState(ipa:delete_cascade(), "FkC", "1"),
+  tutils:assertState(ipa:delete_cascade(), "FkB", "2").
+
+load_multilevel_data() ->
+  {ok, []} = tutils:aql("INSERT INTO FkA VALUES (1)"),
+  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
+  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (2, 1)"),
+  {ok, []} = tutils:aql("INSERT INTO FkC VALUES (1, 1)"),
+  {ok, []} = tutils:aql("INSERT INTO FkC VALUES (2, 1)").
--- test/tutils.erl
+++ test/tutils.erl
@@ -2,7 +2,33 @@
 
 -module(tutils).
 
--export([aql/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+-export([aql/1,
+          create_single_table/1,
+          create_fk_table/2,
+          delete_by_key/2]).
+
+-export([assertState/3]).
 
 aql(Aql) ->
   aqlparser:parse({str, Aql}).
+
+create_single_table(Name) ->
+  Query = ["CREATE TABLE ", Name, " (ID INT PRIMARY KEY)"],
+  aql(lists:concat(Query)).
+
+create_fk_table(Name, Pointer) ->
+  Query = ["CREATE TABLE ", Name,
+    " (ID INT PRIMARY KEY, ", Pointer, " INT FOREIGN KEY REFERENCES ",
+    Pointer, "(ID))"],
+  aql(lists:concat(Query)).
+
+delete_by_key(TName, Key) ->
+  Query = ["DELETE FROM ", TName, " WHERE ID = ", Key],
+  aql(lists:concat(Query)).
+
+assertState(State, TName, Key) ->
+  Query = ["SELECT * FROM ", TName, " WHERE ID = ", Key],
+  {ok, [Res]} = tutils:aql(lists:concat(Query)),
+  ?assertEqual(State, element:st_value(Res)).

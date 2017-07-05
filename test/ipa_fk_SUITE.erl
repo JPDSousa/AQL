4de3de16d58957cf865a347a10ee763560704f81
Joao <jpd.sousa@campus.fct.unl.pt>
2017-07-05 15:07:20 +0100

    Testing foreign key dependencies

--- test/ipa_fk_SUITE.erl
+++ test/ipa_fk_SUITE.erl
@@ -13,7 +13,8 @@
           all/0]).
 
 -export([insert_basic/1, insert_multilevel/1,
-          delete_basic/1, delete_multilevel/1]).
+          delete_basic/1, delete_multilevel/1,
+          create_table_fail/1]).
 
 init_per_suite(Config) ->
   tutils:create_single_table("FkA"),
@@ -25,25 +26,36 @@ end_per_suite(Config) ->
   Config.
 
 init_per_testcase(_Case, Config) ->
+    {ok, []} = tutils:aql("INSERT INTO FkA VALUES (1)"),
+    {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
+    {ok, []} = tutils:aql("INSERT INTO FkB VALUES (2, 1)"),
+    {ok, []} = tutils:aql("INSERT INTO FkC VALUES (1, 1)"),
+    {ok, []} = tutils:aql("INSERT INTO FkC VALUES (2, 1)"),
     Config.
 
 end_per_testcase(_, _) ->
-ok.
+  {ok, []} = tutils:aql("DELETE FROM FkA WHERE ID = 1"),
+  ok.
 
 all() ->
-  [insert_basic,
-  insert_multilevel,
-  delete_basic,
-  delete_multilevel].
+  [insert_basic, insert_multilevel,
+  delete_basic, delete_multilevel,
+  create_table_fail].
+
+create_table_fail(_Config) ->
+  % cannot create table that points to a non-existant table
+  ?assertThrow(_, tutils:create_fk_table("FkETest", "FkFTest")),
+  % cannot create a table that points to a non-existant column
+  ?assertThrow(_, tutils:create_fk_table("FkETest", "FkA", "ABC")),
+  % canot create a table that points to a non-primary key column
+  ?assertThrow(_, tutils:create_fk_table("FkETest", "FkB", "FkA")).
 
 insert_basic(_Config) ->
   % insert test
   {ok, []} = tutils:aql("INSERT INTO FkA VALUES (1)"),
   tutils:assertState(ipa:new(), "FkA", "1"),
   % touch test
-  io:fwrite("B: ~p~n", [tutils:aql("SELECT * FROM FkB WHERE ID = 1")]),
   {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
-  io:fwrite("A: ~p~n", [tutils:aql("SELECT * FROM FkB WHERE ID = 1")]),
   tutils:assertState(ipa:new(), "FkB", "1"),
   tutils:assertState(ipa:touch(), "FkA", "1"),
   % touch cascade test
@@ -53,7 +65,6 @@ insert_basic(_Config) ->
 
 insert_multilevel(_Config) ->
   %bottom level insert
-  load_multilevel_data(),
   tutils:assertState(ipa:touch(), "FkA", "1"),
   tutils:assertState(ipa:touch(), "FkB", "1"),
   tutils:assertState(ipa:touch_cascade(), "FkB", "2"),
@@ -72,17 +83,9 @@ delete_basic(_Config) ->
   tutils:assertState(ipa:delete_cascade(), "FkB", "1").
 
 delete_multilevel(_Config) ->
-  load_multilevel_data(),
   {ok, []} = tutils:delete_by_key("FkA", "1"),
   tutils:assertState(ipa:delete(), "FkA", "1"),
   tutils:assertState(ipa:delete_cascade(), "FkB", "1"),
   tutils:assertState(ipa:delete_cascade(), "FkB", "2"),
   tutils:assertState(ipa:delete_cascade(), "FkC", "1"),
   tutils:assertState(ipa:delete_cascade(), "FkB", "2").
-
-load_multilevel_data() ->
-  {ok, []} = tutils:aql("INSERT INTO FkA VALUES (1)"),
-  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (1, 1)"),
-  {ok, []} = tutils:aql("INSERT INTO FkB VALUES (2, 1)"),
-  {ok, []} = tutils:aql("INSERT INTO FkC VALUES (1, 1)"),
-  {ok, []} = tutils:aql("INSERT INTO FkC VALUES (2, 1)").

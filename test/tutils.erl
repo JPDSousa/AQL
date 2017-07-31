b8fec10c2c00bf283e3e6b2ac12715f7f0570762
Joao <jpd.sousa@campus.fct.unl.pt>
2017-07-31 18:23:55 +0100

    Added projection utility

--- test/tutils.erl
+++ test/tutils.erl
@@ -7,7 +7,8 @@
 -export([aql/1,
           create_single_table/1,
           create_fk_table/2, create_fk_table/3,
-          delete_by_key/2]).
+          delete_by_key/2,
+          read_keys/3]).
 
 -export([assertState/3]).
 
@@ -36,3 +37,16 @@ assertState(State, TName, Key) ->
   Query = ["SELECT * FROM ", TName, " WHERE ID = ", Key],
   {ok, [Res]} = aql(lists:concat(Query)),
   ?assertEqual(State, element:st_value(Res)).
+
+read_keys(Table, ID, Keys) ->
+  Join = join_keys(Keys, []),
+  Query = ["SELECT ", Join, " FROM ", Table, " WHERE ID = ", ID],
+  {ok, [Res]} = aql(lists:concat(Query)),
+  lists:map(fun({_k, V}) -> V end, Res).
+
+join_keys([Key | Keys], []) ->
+  join_keys(Keys, Key);
+join_keys([Key | Keys], Acc) ->
+  NewAcc = lists:concat([Acc, ", ", Key]),
+  join_keys(Keys, NewAcc);
+join_keys([], Acc) -> Acc.

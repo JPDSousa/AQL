60fdb0650b45fc8209cf78a195c4e51480c229b9
Joao <jpd.sousa@campus.fct.unl.pt>
2017-07-31 18:27:50 +0100

    Revived touch

--- src/insert.erl
+++ src/insert.erl
@@ -18,31 +18,28 @@ exec({Table, Tables}, Props, TxId) ->
 	Keys = get_keys(Table, Props),
 	Values = proplists:get_value(?PROP_VALUES, Props),
 	AnnElement = element:new(Table),
-	{ok, Element} = element:put(Keys, Values, AnnElement),
+	{ok, Element} = element:put(Keys, Values, AnnElement, Tables, TxId),
 	element:insert(Element, TxId),
 	Pk = element:primary_key(Element),
 	index:put(Pk, TxId),
 	% update foreign key references
-	TName = table:name(Table),
-	Fks = element:foreign_keys(Element),
-	lists:foreach(fun (Fk) -> touch(Pk, Fk, TxId) end, Fks).
+	Fks = element:foreign_keys(foreign_keys:from_table(Table), Element),
+	lists:foreach(fun (Fk) -> touch(Fk, Tables, TxId) end, Fks).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-touch({_K, _T, CTabName} = Pk, {CName, {Value, PTabName}}, TxId) ->
+touch({{CName, _CType}, {PTabName, _PTabAttr}, Value}, Tables TxId) ->
 	TKey = element:create_key(Value, PTabName),
 	% TODO check if exists
 	antidote:update_objects(crdt:ipa_update(TKey, ipa:touch()), TxId),
 	{ok, [Element]} = antidote:read_objects(TKey, TxId),
-	% touch cascade children
-	%Refs = proplists:get_value(element:refs_key(), Element, []),
-	%lists:foreach(fun (K) -> touch_cascade(K, TxId) end, Refs),
+	% TODO touch cascade children
 	% touch parents
-	Table = table:lookup(PTabName, TxId),
-	FKs = foreign_keys:from_table(Table),
-	lists:foreach(fun (K) -> touch(TKey, K, TxId) end, foreign_keys:parents(Element, FKs)).
+	Table = table:lookup(PTabName, Tables),
+	Fks = element:foreign_keys(foreign_keys:from_table(Table), Element),
+	lists:foreach(fun (K) -> touch(K, Tables, TxId) end, Fks).
 
 touch_cascade(Key, TxId) ->
 	antidote:update_objects(crdt:ipa_update(Key, ipa:touch_cascade()), TxId),
@@ -54,7 +51,7 @@ get_keys(Table, Props) ->
 	Clause = proplists:get_value(?PROP_COLUMNS, Props),
 	case Clause of
 		?PARSER_WILDCARD ->
-			Keys = table:get_col_names(Table),
+			Keys = column:s_names(Table),
 			lists:map(fun (V) -> ?PARSER_ATOM(V) end, Keys);
 		_Else ->
 			Clause

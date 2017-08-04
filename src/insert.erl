b986f7a668824fdd976017510dfd1c1207dd9ec4
Joao <jpd.sousa@campus.fct.unl.pt>
2017-08-04 12:13:28 +0100

    Touch-cascade

--- src/insert.erl
+++ src/insert.erl
@@ -23,6 +23,7 @@ exec({Table, Tables}, Props, TxId) ->
 	Pk = element:primary_key(Element),
 	index:put(Pk, TxId),
 	% update foreign key references
+	touch_cascade(Element, TxId),
 	Fks = element:foreign_keys(foreign_keys:from_table(Table), Element),
 	lists:foreach(fun (Fk) -> touch(Fk, Tables, TxId) end, Fks).
 
@@ -35,17 +36,38 @@ touch({_Col, {PTabName, _PTabAttr}, Value}, Tables, TxId) ->
 	% TODO check if exists
 	antidote:update_objects(crdt:ipa_update(TKey, ipa:touch()), TxId),
 	{ok, [Element]} = antidote:read_objects(TKey, TxId),
-	% TODO touch cascade children
-	% touch parents
 	Table = table:lookup(PTabName, Tables),
+	% touch cascade
+	touch_cascade(Element, Table, PTabName, TxId),
+	% touch parents
 	Fks = element:foreign_keys(foreign_keys:from_table(Table), Element, PTabName),
 	lists:foreach(fun (K) -> touch(K, Tables, TxId) end, Fks).
 
-touch_cascade(Key, TxId) ->
-	antidote:update_objects(crdt:ipa_update(Key, ipa:touch_cascade()), TxId),
-	{ok, [Element]} = antidote:read_objects(Key, TxId),
-	Refs = proplists:get_value(element:refs_key(), Element, []),
-	lists:foreach(fun (K) -> touch_cascade(K, TxId) end, Refs).
+touch_cascade(Element, TxId) when is_tuple(Element) ->
+	Data = element:data(Element),
+	Cols = element:attributes(Element),
+	TName = element:table(Element),
+	touch_cascade(Data, Cols, TName, TxId).
+
+touch_cascade(Data, Table, TName, TxId) when is_list(Table) ->
+	Cols = column:s_from_table(Table),
+	touch_cascade(Data, Cols, TName, TxId);
+touch_cascade(Data, Cols, TName, TxId) ->
+	Fks = foreign_keys:from_columns(Cols),
+	ShadowCols = foreign_keys:shadow_cols(Data),
+	FkOps = lists:map(fun({{FkName, AQL}, _Parent}) ->
+		Value = element:get(FkName, types:to_crdt(AQL), Data, TName),
+		index:tag(TName, FkName, Value, ipa:touch_cascade())
+	end, Fks),
+	ShadowOps = lists:map(fun({{K, _Type}, V}) ->
+		index:tag(TName, K, V, ipa:touch_cascade())
+	end, ShadowCols),
+	Ops = lists:append(FkOps, ShadowOps),
+	case Ops of
+		[] -> ok;
+		_Else ->
+			antidote:update_objects(Ops, TxId)
+	end.
 
 get_keys(Table, Props) ->
 	Clause = proplists:get_value(?PROP_COLUMNS, Props),

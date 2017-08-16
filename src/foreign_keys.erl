4d1b143e0ed1f5f2dbf02cef58f43e142ccc73a0
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-08-16 10:45:22 +0100

    Removed load chain

--- src/foreign_keys.erl
+++ src/foreign_keys.erl
@@ -7,11 +7,14 @@
 
 -include("aql.hrl").
 -include("parser.hrl").
+-include("types.hrl").
 
 -export([from_table/1,
 			from_columns/1,
-			load_chain/4
-			]).
+			to_cname/1]).
+
+from_table(Table) ->
+	from_columns(table:columns(Table)).
 
 from_column(Column) ->
 	Name = column:name(Column),
@@ -20,28 +23,28 @@ from_column(Column) ->
 	?FOREIGN_KEY({TName, Attr}) = Constraint,
 	?T_FK(Name, Type, TName, Attr).
 
-from_table(Table) ->
-	from_columns(column:s_from_table(Table)).
-
 from_columns(Columns) ->
 	Fks = maps:filter(fun (_CName, Col) ->
 		column:is_foreign_key(Col)
 	end, Columns),
-	FkList = dict:to_list(Fks),
+	FkList = maps:to_list(Fks),
 	lists:map(fun ({_Name, Column}) ->
 		from_column(Column)
 	end, FkList).
 
-load_chain([{CName, TName} | FkChain], Value, Tables, TxId) ->
-	Table = table:lookup(TName, Tables),
-	Fks = from_table(Table),
-	{ok, [Parent]} = antidote:read_objects(element:create_key(Value, TName), TxId),
-	Unflat = lists:map(fun ({{_Key, FkType}, {FkTable, FkAttr}}) ->
-		FkName = [{FkAttr, FkTable}] ++ [{CName, TName}] ++ FkChain,
-		FkValue = element:get(FkAttr, types:to_crdt(FkType), Parent, TName),
-		[{FkName, FkType, FkValue} | load_chain(FkName, FkValue, Tables, TxId)]
-	end, Fks),
-	lists:flatten(Unflat).
+to_cname([{_TName, CName}]) -> CName;
+to_cname(ShCName) -> ShCName.
+
+%load_chain([{CName, TName} | FkChain], Value, Tables, TxId) ->
+%	Table = table:lookup(TName, Tables),
+%	Fks = from_table(Table),
+%	{ok, [Parent]} = antidote:read_objects(element:create_key(Value, TName), TxId),
+%	Unflat = lists:map(fun ({{_Key, FkType}, {FkTable, FkAttr}}) ->
+%		FkName = [{FkAttr, FkTable}] ++ [{CName, TName}] ++ FkChain,
+%		FkValue = element:get(FkAttr, types:to_crdt(FkType), Parent, TName),
+%		[{FkName, FkType, FkValue} | load_chain(FkName, FkValue, Tables, TxId)]
+%	end, Fks),
+%	lists:flatten(Unflat).
 
 %%====================================================================
 %% Eunit tests

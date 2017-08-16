891e731adf101f1eb896e04241bc0ba549dd10da
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-08-16 10:57:42 +0100

    Delete cascade

--- src/delete.erl
+++ src/delete.erl
@@ -20,7 +20,7 @@ exec({Table, Tables}, Props, TxId) ->
 	Keys = where:scan(TName, Condition, TxId),
 	lists:foreach(fun (Key) ->
 		antidote:update_objects(crdt:ipa_update(Key, ipa:delete()), TxId),
-		delete_cascade(Key, TName, Tables, TxId)
+		delete_cascade(Key, Table, Tables, TxId)
 	end, Keys).
 
 table({TName, _Where}) -> TName.
@@ -31,17 +31,13 @@ where({_TName, Where}) -> Where.
 %% Internal functions
 %% ====================================================================
 
-delete_cascade(Key, TName, Tables, TxId) ->
+delete_cascade(Key, Table, Tables, TxId) ->
+	TName = table:name(Table),
 	{ok, [Data]} = antidote:read_objects(Key, TxId),
-	DepTables = table:dependents(TName, Tables),
-	Ops = lists:map(fun({DepTName, DepCols}) ->
-		lists:map(fun({[{_TName, CName} | Chain]} = DepCol) ->
-			Value = element:get_by_name(CName, Data),
-			index:tag(DepTName, DepCol, Value, ipa:delete_cascade())
-		end, DepCols)
-	end, DepTables),
-	case Ops of
-		[] -> ok;
-		_Else ->
-			antidote:update_objects(lists:flatten(Ops), TxId)
-	end.
+	Refs = table:dependants(TName, Tables),
+	lists:foreach(fun({RefTName, RefCols}) ->
+		lists:foreach(fun(?T_FK(FkName, FkType, _TName, CName)) ->
+			Value = element:get(CName, types:to_crdt(FkType), Data, Table),
+			index:tag(RefTName, FkName, Value, ipa:delete_cascade(), TxId)
+		end, RefCols)
+	end, Refs).

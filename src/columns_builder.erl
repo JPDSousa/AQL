91a76b37f629dfca80b9eac6e0975c863ab10101
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-08-16 10:08:05 +0100

    Added tests to columns_builder module

--- src/columns_builder.erl
+++ src/columns_builder.erl
@@ -1,6 +1,14 @@
 
 -module(columns_builder).
 
+-include("aql.hrl").
+-include("parser.hrl").
+-include("types.hrl").
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -export([new/0,
         put_raw/2,
         build/1]).
@@ -23,3 +31,33 @@ put_raw(Col, {Maps, Names, Pks}) ->
 build({Maps, Names, Pks}) ->
   Build = maps:put(?C_NAMES, Names, Maps),
   maps:put(?C_PK, Pks, Build).
+
+%%====================================================================
+%% Eunit tests
+%%====================================================================
+
+-ifdef(TEST).
+
+new_test() ->
+  ?assertEqual({maps:new(), [], []}, new()).
+
+put_raw_test() ->
+  Col = ?T_COL("Test", ?AQL_INTEGER, ?NO_CONSTRAINT),
+  Expected = {#{"Test" => Col}, ["Test"], []},
+  ?assertEqual(Expected, put_raw(Col, new())).
+
+put_raw_pk_test() ->
+  Col = ?T_COL("Test", ?AQL_INTEGER, ?PRIMARY_TOKEN),
+  Expected = {#{"Test" => Col}, ["Test"], ["Test"]},
+  ?assertEqual(Expected, put_raw(Col, new())).
+
+build_test() ->
+  Col1 = ?T_COL("Test1", ?AQL_INTEGER, ?PRIMARY_TOKEN),
+  Col2 = ?T_COL("Test2", ?AQL_VARCHAR, ?NO_CONSTRAINT),
+  Expected = #{"Test1" => Col1, "Test2" => Col2, ?C_NAMES => ["Test1", "Test2"], ?C_PK => ["Test1"]},
+  Actual1 = new(),
+  Actual2 = put_raw(Col1, Actual1),
+  Actual3 = put_raw(Col2, Actual2),
+  ?assertEqual(Expected, build(Actual3)).
+
+-endif.

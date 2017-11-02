405d9fc82d3e2675fca4fea3916d2617ca346bd0
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-02 12:09:01 +0000

    added pn_counter support

--- include/aql.hrl
+++ include/aql.hrl
@@ -22,7 +22,8 @@
 -define(CRDT_BOOLEAN, antidote_crdt_flag_ew).
 
 -define(AQL_COUNTER_INT, counter_int).
--define(CRDT_COUNTER_INT, antidote_crdt_bcounter).
+-define(CRDT_BCOUNTER_INT, antidote_crdt_bcounter).
+-define(CRDT_COUNTER_INT, antidote_crdt_counter).
 
 % types
 -export_type([]).%complete with private types
